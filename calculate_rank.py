2aeeadb94ee7c2fdd30a0defe505baf5c22d63cd
Ninnna <39150379+Ninnna@users.noreply.github.com>
2018-05-25 18:08:18 +1000

    Add files via upload

--- calculate_rank.py
+++ calculate_rank.py
@@ -1,5 +1,5 @@
 from math import radians, cos, sin, asin, sqrt
-
+import json
 from mongoengine import connect,StringField,Document, EmbeddedDocument,ListField, EmbeddedDocumentField,IntField
 
 class Crime_info(EmbeddedDocument):
@@ -86,20 +86,43 @@ def get_rank(location):
     all_distance = need[0]
     all_crime = need[1]
     all_score = need[2]
-    rank_school = dict()
+    res = []
+    second = []
+    average = dict()
+    distance_avg = 0
+    score_avg = 0
+    crime_avg = 0
     for i in Selective.objects:
+        school_info = dict()
+        school_info['school_name'] = i.school_name
         for j in i.school_info:
             distance = geodistance(a[0], a[1], float(j.longitude), float(j.latitude))
             distance_weight = (max(all_distance) - distance) / max(all_distance) * 4
             crime_weight = (max(all_crime) - float(j.average_crime)) / max(all_crime) * 3
             score_weight = float(j.average_score) / max(all_score) * 3
-            value = []
-            value.append(distance_weight + crime_weight + score_weight)
-            value.append(j.longitude)
-            value.append(j.latitude)
-            rank_school[i.school_name] = value
-    return rank_school
+            school_info['longitude'] = j.longitude
+            school_info['latitude'] = j.latitude
+            school_info['distance_rate'] = (max(all_distance) - distance) / max(all_distance) * 10
+            distance_avg += school_info['distance_rate']
+            school_info['score_rate'] = float(j.average_score) / max(all_score) * 10
+            score_avg += school_info['score_rate']
+            school_info['crime_rate'] = (max(all_crime) - float(j.average_crime)) / max(all_crime) * 10
+            crime_avg += school_info['crime_rate']
+            school_info['total_rate'] = distance_weight + crime_weight + score_weight
+        res.append(school_info)
+    distance_avg = distance_avg / 40
+    score_avg = score_avg / 40
+    crime_avg = score_avg / 40
+    average['distance_average'] = distance_avg
+    average['score_average'] = score_avg
+    average['crime_average'] = crime_avg
+    second.append(average)
+    return [res, second]
 
 c = get_rank(a)
-c = sorted(c.items(), key = lambda x : x[1], reverse = True)
-print(c)
+
+result = sorted(c[0], key = lambda x : x['total_rate'], reverse = True)
+result = json.dumps(result, indent= 4)
+print(result)
+
+print(c[1])
